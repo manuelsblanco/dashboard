26fda9ac9ca5268284be5b63ad7d8a2199560907
Manuel Sebastian Blanco <mannywhite89@icloud.com>
2024-01-29 15:06:24 +0100

    Update chart data and license in javascript and HTML files
    Updated the javascript code in 'charts.js' to include the 'Xfail' variabe in the chart data. Also, altered the copyright and license information in 'audit-chz-desktop.html' and updated the favicon. This adjustment helps track the 'Xfail' test data and conforms the license information to the recent guidelines.

--- javascript/charts.js
+++ javascript/charts.js
@@ -11,28 +11,36 @@ window.addEventListener('DOMContentLoaded', (event) => {
                     let parser = new DOMParser();
                     let doc = parser.parseFromString(data, 'text/html');
 
-                    let failed = doc.querySelector('.failed').textContent;
-                    let passed = doc.querySelector('.passed').textContent;
-                    let skipped = doc.querySelector('.skipped').textContent;
+                    let failedElement = doc.querySelector('.failed');
+                    let passedElement = doc.querySelector('.passed');
+                    let skippedElement = doc.querySelector('.skipped');
+                    let xfailElement = doc.querySelector('.xfailed');
+
+                    let failed = failedElement?.textContent || "0";
+                    let passed = passedElement?.textContent || "0";
+                    let skipped = skippedElement?.textContent || "0";
+                    let xfail = xfailElement?.textContent || "0";
 
                     let numFailed = Number(failed.split(" ")[0]);
                     let numPassed = Number(passed.split(" ")[0]);
                     let numSkipped = Number(skipped.split(" ")[0]);
+                    let numXfail = Number(xfail.split(" ")[0]);
                     let title = fileNames[i].split('.')[0];
 
-                    drawChart({numFailed, numPassed, numSkipped, title});
+                    drawChart({numFailed, numPassed, numSkipped, numXfail, title});
                 });
             });
 
-            function drawChart({numFailed, numPassed, numSkipped, title}) {
+            function drawChart({numFailed, numPassed, numSkipped, numXfail,title}) {
                 const data = google.visualization.arrayToDataTable([
                     ['Result', 'Number of tests'],
                     ['Failed', numFailed],
                     ['Passed', numPassed],
-                    ['Skipped', numSkipped]
+                    ['Skipped', numSkipped],
+                    ['Xfail', numXfail]
                 ]);
 
-                const options = {colors: ['#FF0000', '#178017', '#c2c238']};
+                const options = {colors: ['#FF0000', '#178017', '#c2c238','#000000']};
 
                 const targetElement = document.getElementById(title + '-results');
                 const chartDiv = document.createElement('div');
