38dedb8d806e497c8fad85da256394390143d8b4
Manuel Sebastian Blanco <mannywhite89@icloud.com>
2024-02-01 18:06:11 +0100

    Refactor chart generation and JSON processing
    The code has been refactored to load and process JSON files individually, generating a unique chart per dataset, instead of aggregating all data first. This optimizes the data processing solution with less redundancy, allowing for a more flexible, efficient, and scalable system that can handle an increased amount of different datasets in the future. Changes also include removal of global variables for better code clarity and separation of concerns among functions.

--- javascript/columns.js
+++ javascript/columns.js
@@ -1,55 +1,55 @@
-// Objeto global para almacenar los datos procesados
-var lighthouseData = {};
+document.addEventListener("DOMContentLoaded", function() {
+  const files = [
+    { filename: 'audit-chz-desktop.report.json', prefix: 'chzDesktop' },
+    { filename: 'audit-chz-mobile.report.json', prefix: 'chzMobile' },
+    { filename: 'audit-ebw-desktop.report.json', prefix: 'ebwDesktop' },
+    { filename: 'audit-ebw-mobile.report.json', prefix: 'ebwMobile' },
+    { filename: 'audit-crd-desktop.report.json', prefix: 'crdDesktop' },
+    { filename: 'audit-crd-mobile.report.json', prefix: 'crdMobile' },
+    { filename: 'audit-kym-desktop.report.json', prefix: 'kymDesktop' },
+    { filename: 'audit-kym-mobile.report.json', prefix: 'kymMobile' }
+  ];
 
-function parseLighthouseData(json, prefix) {
-  try {
-    if (!json.categories || !json.categories.accessibility || !json.categories['best-practices'] || !json.categories.performance || !json.categories.seo) {
-      throw new Error('Missing categories in JSON data');
-    }
+  files.forEach(file => {
+    loadAndProcessJSON(file.filename, file.prefix);
+  });
+});
 
-    const accessibilityScore = json.categories.accessibility.score;
-    const bestPracticesScore = json.categories['best-practices'].score;
-    const performanceScore = json.categories.performance.score;
-    const seoScore = json.categories.seo.score;
-
-    const accessibility = accessibilityScore * 100;
-    const bestPractices = bestPracticesScore * 100;
-    const performance = performanceScore * 100;
-    const seo = seoScore * 100;
-
-    // Almacenar los datos procesados en el objeto global
-    lighthouseData[prefix] = { accessibility, bestPractices, performance, seo };
-
-    return { accessibility, bestPractices, performance, seo };
-  } catch (error) {
-    console.error(`Error parsing Lighthouse data for prefix ${prefix}:`, error);
-  }
+// Reutiliza esta función para cada archivo JSON
+function loadAndProcessJSON(filename, prefix) {
+  fetch(`./lighthouse/${filename}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Failed to fetch ${filename}: ${response.status} ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then(jsonData => {
+        const parsedData = parseLighthouseData(jsonData, prefix);
+        // Cuando los datos se han procesado, pasa los datos procesados y el prefijo al generador de gráficos
+        generateChart(parsedData, prefix);
+      })
+      .catch(error => console.error(`Error fetching ${filename}:`, error));
 }
 
-function generateCharts() {
-  // Seleccionar el elemento canvas donde se renderizarán los gráficos
-  var ctx = document.getElementById('myChart').getContext('2d');
+// Utilice generateChart en lugar de generateCharts
+function generateChart(data, prefix) {
+  const ctx = document.getElementById(prefix + 'Canvas').getContext('2d');
 
   // Configuración de los datos para el gráfico
-  var data = {
+  const chartData = {
     labels: ['Accessibility', 'Best Practices', 'Performance', 'SEO'],
-    datasets: []
-  };
-
-  // Recorrer los datos almacenados en lighthouseData y agregarlos al conjunto de datos del gráfico
-  Object.keys(lighthouseData).forEach(prefix => {
-    var dataset = {
+    datasets: [{
       label: prefix,
-      backgroundColor: getRandomColor(), // Función para obtener un color aleatorio
+      backgroundColor: getRandomColor(),
       data: [
-        lighthouseData[prefix].accessibility,
-        lighthouseData[prefix].bestPractices,
-        lighthouseData[prefix].performance,
-        lighthouseData[prefix].seo
+        data.accessibility,
+        data.bestPractices,
+        data.performance,
+        data.seo
       ]
-    };
-    data.datasets.push(dataset);
-  });
+    }]
+  };
 
   // Configuración del gráfico
   var options = {
@@ -66,22 +66,34 @@ function generateCharts() {
   };
 
   // Crear el gráfico de barras horizontales
-  var myChart = new Chart(ctx, {
+  const myChart = new Chart(ctx, {
     type: 'horizontalBar',
-    data: data,
+    data: chartData,
     options: options
   });
+}
+function parseLighthouseData(json, prefix) {
+  try {
+    if (!json.categories || !json.categories.accessibility || !json.categories['best-practices'] || !json.categories.performance || !json.categories.seo) {
+      throw new Error('Missing categories in JSON data');
+    }
 
-  // Imprimir los gráficos en el HTML
-  Object.keys(lighthouseData).forEach(prefix => {
-    document.getElementById(prefix + 'Performance').textContent = lighthouseData[prefix].performance.toFixed();
-    document.getElementById(prefix + 'Accessibility').textContent = lighthouseData[prefix].accessibility.toFixed();
-    document.getElementById(prefix + 'BestPractices').textContent = lighthouseData[prefix].bestPractices.toFixed();
-    document.getElementById(prefix + 'SEO').textContent = lighthouseData[prefix].seo.toFixed();
-  });
+    const accessibilityScore = json.categories.accessibility.score;
+    const bestPracticesScore = json.categories['best-practices'].score;
+    const performanceScore = json.categories.performance.score;
+    const seoScore = json.categories.seo.score;
+
+    const accessibility = accessibilityScore * 100;
+    const bestPractices = bestPracticesScore * 100;
+    const performance = performanceScore * 100;
+    const seo = seoScore * 100;
+
+    return { accessibility, bestPractices, performance, seo };
+  } catch (error) {
+    console.error(`Error parsing Lighthouse data for prefix ${prefix}:`, error);
+  }
 }
 
-// Función para obtener un color aleatorio
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
   var color = '#';
@@ -90,34 +102,3 @@ function getRandomColor() {
   }
   return color;
 }
-
-function loadAndProcessJSON(filename, prefix) {
-  fetch(`./lighthouse/${filename}`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Failed to fetch ${filename}: ${response.status} ${response.statusText}`);
-        }
-        return response.json();
-      })
-      .then(jsonData => {
-        const parsedData = parseLighthouseData(jsonData, prefix);
-        // Llamar a la función para generar los gráficos después de procesar los datos de cada archivo JSON
-        generateCharts();
-      })
-      .catch(error => console.error(`Error fetching ${filename}:`, error));
-}
-
-const files = [
-  { filename: 'audit-chz-desktop.report.json', prefix: 'chzDesktop' },
-  { filename: 'audit-chz-mobile.report.json', prefix: 'chzMobile' },
-  { filename: 'audit-ebw-desktop.report.json', prefix: 'ebwDesktop' },
-  { filename: 'audit-ebw-mobile.report.json', prefix: 'ebwMobile' },
-  { filename: 'audit-crd-desktop.report.json', prefix: 'crdDesktop' },
-  { filename: 'audit-crd-mobile.report.json', prefix: 'crdMobile' },
-  { filename: 'audit-kym-desktop.report.json', prefix: 'kymDesktop' },
-  { filename: 'audit-kym-mobile.report.json', prefix: 'kymMobile' }
-];
-
-files.forEach(file => {
-  loadAndProcessJSON(file.filename, file.prefix);
-});
--- javascript/data.js
+++ javascript/data.js
@@ -1,30 +1,32 @@
-const fileNames = ['pytest/chz.html', 'pytest/ebw.html', 'pytest/crd.html', 'pytest/kym.html'];
-const fileNames2 = ['chz.html', 'ebw.html', 'crd.html', 'kym.html'];
+document.addEventListener("DOMContentLoaded", function() {
+    const fileNames = ['pytest/chz.html', 'pytest/ebw.html', 'pytest/crd.html', 'pytest/kym.html'];
+    const fileNames2 = ['chz.html', 'ebw.html', 'crd.html', 'kym.html'];
 
-const fetchPromises = fileNames.map(fileName => fetch(fileName).then(response => response.text()));
+    const fetchPromises = fileNames.map(fileName => fetch(fileName).then(response => response.text()));
 
-Promise.all(fetchPromises)
-    .then(responses => {
-        responses.forEach((htmlString, index) => {
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(htmlString, 'text/html');
+    Promise.all(fetchPromises)
+        .then(responses => {
+            responses.forEach((htmlString, index) => {
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(htmlString, 'text/html');
 
-            const failedElement = doc.querySelector('.failed');
-            const passedElement = doc.querySelector('.passed');
-            const skippedElement = doc.querySelector('.skipped');
-            const xfailedElement = doc.querySelector('.xfailed');
+                const failedElement = doc.querySelector('.failed');
+                const passedElement = doc.querySelector('.passed');
+                const skippedElement = doc.querySelector('.skipped');
+                const xfailedElement = doc.querySelector('.xfailed');
 
-            const failed = failedElement ? parseInt(failedElement.textContent.match(/\d+/)[0], 10) : 0;
-            const passed = passedElement ? parseInt(passedElement.textContent.match(/\d+/)[0], 10) : 0;
-            const skipped = skippedElement ? parseInt(skippedElement.textContent.match(/\d+/)[0], 10) : 0;
-            const xfailed = xfailedElement ? parseInt(xfailedElement.textContent.match(/\d+/)[0], 10) : 0;
+                const failed = failedElement ? parseInt(failedElement.textContent.match(/\d+/)[0], 10) : 0;
+                const passed = passedElement ? parseInt(passedElement.textContent.match(/\d+/)[0], 10) : 0;
+                const skipped = skippedElement ? parseInt(skippedElement.textContent.match(/\d+/)[0], 10) : 0;
+                const xfailed = xfailedElement ? parseInt(xfailedElement.textContent.match(/\d+/)[0], 10) : 0;
 
-            const key = fileNames2[index].split('.')[0].toUpperCase();
+                const key = fileNames2[index].split('.')[0].toUpperCase();
 
-            document.getElementById(`totalTest-${key}`).textContent = failed + passed + skipped + xfailed;
-            document.getElementById(`passed-${key}`).textContent = passed;
-            document.getElementById(`failed-${key}`).textContent = failed;
-            document.getElementById(`xfailed-${key}`).textContent = xfailed;
-            document.getElementById(`skipped-${key}`).textContent = skipped;
+                document.getElementById(`totalTest-${key}`).textContent = failed + passed + skipped + xfailed;
+                document.getElementById(`passed-${key}`).textContent = passed;
+                document.getElementById(`failed-${key}`).textContent = failed;
+                document.getElementById(`xfailed-${key}`).textContent = xfailed;
+                document.getElementById(`skipped-${key}`).textContent = skipped;
+            });
         });
-    });
+});
