9dfa6d005275daa1d162d8f4fb235707ae4e4850
Manuel Sebastian Blanco <mannywhite89@icloud.com>
2024-02-01 21:19:06 +0100

    Improve chart visualization with score-based colors
    The chart configuration in 'columns.js' now uses score-based colors rather than random colors, making data easier to interpret. Other enhancements include changing the chart type to horizontal bars for better data presentation, and updates to scales, axes and labels for more accurate chart rendering.

--- javascript/columns.js
+++ javascript/columns.js
@@ -36,69 +36,94 @@ function loadAndProcessJSON(filename, prefix) {
 function generateChart(data, prefix) {
   const ctx = document.getElementById(prefix + 'Canvas').getContext('2d');
 
-  // Configuración de los datos para el gráfico
-  const chartData = {
-    labels: ['Accessibility', 'Best Practices', 'Performance', 'SEO'],
-    datasets: [{
-      label: prefix,
-      backgroundColor: getRandomColor(),
-      data: [
-        data.accessibility,
-        data.bestPractices,
-        data.performance,
-        data.seo
-      ]
-    }]
-  };
-
   // Configuración del gráfico
-  var options = {
+  const options = {
+    indexAxis: 'y', // Cambio aquí para hacer el gráfico horizontal
     scales: {
-      xAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }],
-      yAxes: [{
+      x: {
+        beginAtZero: true
+      },
+      y: {
         stacked: true
-      }]
+      }
     }
   };
 
+  // Configuración de los datos para el gráfico
+  const chartData = {
+    labels: ['Performance', 'Accessibility', 'Best Practices', 'SEO'],
+    datasets: [{
+      label: prefix,
+      data: [
+        data.performance,
+        data.accessibility,
+        data.bestPractices,
+        data.seo
+      ],
+      backgroundColor: [
+        getColorForScore(data.performance),
+        getColorForScore(data.accessibility),
+        getColorForScore(data.bestPractices),
+        getColorForScore(data.seo)
+      ],
+      borderColor: [
+        getBorderColorForScore(data.performance),
+        getBorderColorForScore(data.accessibility),
+        getBorderColorForScore(data.bestPractices),
+        getBorderColorForScore(data.seo)
+      ],
+      borderWidth: 0,
+      borderRadius: 10 // Ajusta el valor del radio de borde según tu preferencia
+    }]
+  };
+
   // Crear el gráfico de barras horizontales
-  const myChart = new Chart(ctx, {
-    type: 'horizontalBar',
+  const config = {
+    type: 'bar',
     data: chartData,
     options: options
-  });
+  };
+
+  // Crear el gráfico con Chart.js
+  const myChart = new Chart(ctx, config);
+
+  document.getElementById(prefix + 'Canvas')
+  document.getElementById(prefix + 'Canvas')
 }
+
 function parseLighthouseData(json, prefix) {
   try {
     if (!json.categories || !json.categories.accessibility || !json.categories['best-practices'] || !json.categories.performance || !json.categories.seo) {
       throw new Error('Missing categories in JSON data');
     }
 
-    const accessibilityScore = json.categories.accessibility.score;
-    const bestPracticesScore = json.categories['best-practices'].score;
-    const performanceScore = json.categories.performance.score;
-    const seoScore = json.categories.seo.score;
+    const accessibilityScore = json.categories.accessibility.score * 100;
+    const bestPracticesScore = json.categories['best-practices'].score * 100;
+    const performanceScore = json.categories.performance.score * 100;
+    const seoScore = json.categories.seo.score * 100;
 
-    const accessibility = accessibilityScore * 100;
-    const bestPractices = bestPracticesScore * 100;
-    const performance = performanceScore * 100;
-    const seo = seoScore * 100;
-
-    return { accessibility, bestPractices, performance, seo };
+    return { accessibility: accessibilityScore, bestPractices: bestPracticesScore, performance: performanceScore, seo: seoScore };
   } catch (error) {
     console.error(`Error parsing Lighthouse data for prefix ${prefix}:`, error);
   }
 }
 
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+function getColorForScore(score) {
+  if (score <= 49) {
+    return '#f33'; // Rojo para valores bajos
+  } else if (score <= 89) {
+    return '#fa3'; // Amarillo para valores intermedios
+  } else {
+    return '#0c6'; // Verde para valores altos
   }
-  return color;
+}
+
+function getBorderColorForScore(score) {
+  if (score <= 49) {
+    return '#f33'; // Rojo para valores bajos
+  } else if (score <= 89) {
+    return '#fa3'; // Amarillo para valores intermedios
+  } else {
+    return '#0c6'; // Verde para valores altos
+  }
 }
