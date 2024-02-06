dc556bdfa07dad36bb1588c720377b9fd3e599dd
Manuel Sebastian Blanco <mannywhite89@icloud.com>
2024-02-06 20:54:50 +0100

    Add bot for monitoring and reporting test results
    The newly created bot.py regularly checks for updates on 'EBW' and 'CHZ' test result JSON files and posts their summary and details of any failed tests to Slack. The bot provides a convenient way to monitor test progress and identify any issues promptly.

---
+++ bot/bot.py
@@ -0,0 +1,80 @@
+from time import sleep
+
+import requests
+
+url_CHZ_JSON = 'https://cheezburger.github.io/literally-qa-automation-results/pytest/chz.json'
+url_EBW_JSON = 'https://cheezburger.github.io/literally-qa-automation-results/pytest/ebw.json'
+date_EBW = ''
+date_CHZ = ''
+
+url_slack = 'https://hooks.slack.com/services/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'
+
+
+def bot_message(message):
+    requests.post(url_slack, json={'text': message})
+    print(message)
+
+
+def obtain_report_date(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if data:
+            report_date = data.get("report", {}).get("created_at", "")
+            return report_date
+
+
+def obtain_report_performance(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if data:
+            summary = data.get("report", {}).get("summary", {})
+            if summary:
+                bot_message("Report Summary:")
+                for key, value in summary.items():
+                    bot_message(f"{key.capitalize()}: {value}")
+                total_tests = summary.get('num_tests', 0) - summary.get('skipped', 0)
+                if total_tests > 0:
+                    failed = summary.get('failed', 0)
+                    failed_percentage = round((failed / total_tests) * 100, 2)
+                    bot_message(f"Effectivity of Test Audit: {100 - failed_percentage}%")
+                else:
+                    bot_message("No tests run.")
+            else:
+                bot_message("Summary not found in the JSON report")
+        else:
+            bot_message("Data not found in the JSON report")
+
+
+def failed_details(url, site):
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if data:
+            tests = data.get("report", {}).get("tests", [])
+            if tests:
+                failed_tests = [test for test in tests if test.get('outcome') == 'failed']
+                if failed_tests:
+                    bot_message(f"Failed Test Details: {len(failed_tests)} failed tests for {site} at {obtain_report_date(url)}")
+                    for test in failed_tests:
+                        bot_message(f"Test Name: {test.get('name')}")
+                        bot_message("--------------------")
+                else:
+                    bot_message(f"🤖 Congratulations all the test passed for {site} at {obtain_report_date(url)}")
+            else:
+                bot_message("No tests found in the report 🤔.")
+        else:
+            bot_message("Data not found in the JSON report 😳")
+
+
+while True:
+    sleep(600)
+    if date_EBW != obtain_report_date(url_EBW_JSON):
+        date_EBW = obtain_report_date(url_EBW_JSON)
+        failed_details(url_EBW_JSON, 'EBW')
+
+    if date_CHZ != obtain_report_date(url_CHZ_JSON):
+        date_CHZ = obtain_report_date(url_CHZ_JSON)
+        failed_details(url_CHZ_JSON, 'CHZ 🐱')
+
